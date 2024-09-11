4b41c8421badfbad2ae451843e5c069ac85e52c0
Tinapen <cristinapenaflor20@gmail.com>
2024-09-11 17:12:53 +0800

    final fixes and updates

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { AboutContent } from "../components/AboutContent";
+
+export const About = () => {
+  return (
+    <div className="bg-gray-800">
+      <Header />
+      <div id="about" className="mx-[500px] pt-[100px]">
+        <h1 className="text-center text-white font-bold text-4xl mb-[16px]">
+          ABOUT
+        </h1>
+        <AboutContent />
+      </div>
+      <Footer />
+    </div>
+  );
+};
