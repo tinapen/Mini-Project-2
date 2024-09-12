a9818423ed03569c6a82de8ac0874dfddb6922b2
Tinapen <cristinapenaflor20@gmail.com>
2024-09-13 00:58:57 +0800

    bug fixing

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -4,6 +4,7 @@ export const Preview = ({ track, artistName }) => {
   // variables
   const [previewURL, setPreviewURL] = useState("");
   const [previewPrompt, setPreviewPrompt] = useState("");
+
   //functions
   const getPreview = async () => {
     // Fetching API
@@ -37,31 +38,6 @@ export const Preview = ({ track, artistName }) => {
   };
 
   return (
-    // <div
-    //   id="preview"
-    //   className="container bg-white border-gray-200 dark:bg-gray-800"
-    // >
-    //   <div className=" ms-[370px] flex ">
-    //     {/* Preview prompt button */}
-    //     <button
-    //       onClick={getPreview}
-    //       type="button"
-    //       className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
-    //     >
-    //       Click here to enable song preview
-    //     </button>
-    //     {/* Preview prompt field  */}
-    //     <div className="ms-5 text-red-500 font-bold">{previewPrompt}</div>
-    //   </div>
-    //   {/* Audio output  */}
-    //   <div id="audio-container" className="justify-center align-center flex">
-    //     <audio
-    //       className="w-[50vw] mb-[100px]"
-    //       src={previewURL}
-    //       controls
-    //     ></audio>
-    //   </div>
-    // </div>
     <div className="bg-gray-50 dark:bg-gray-800 w-[100vw] h-[20vh] px-[25%]">
       <div id="preview-btn-prompt" className="flex">
         <button
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { Footer } from "../components/Footer";
 export const Home = () => {
   const [artistName, setArtistName] = useState("");
   const [track, setTrack] = useState("");
+
   return (
     <div id="home-container" className="bg-gray-50 dark:bg-gray-800">
       <Header />
