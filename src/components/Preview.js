8d175ab04cf8f3e76ecfd40bc3f2cee6b4ecd297
Tinapen <cristinapenaflor20@gmail.com>
2024-09-13 19:24:27 +0800

    debugging

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -55,7 +55,7 @@ export const Preview = ({ track, artistName, songTitle }) => {
               type="button"
               className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
             >
-              Reset song preview and prompt
+              Reset song preview
             </span>
           ) : (
             <span
